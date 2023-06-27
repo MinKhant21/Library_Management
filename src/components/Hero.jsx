2c550b618d6b7907828a46cb17dab0bb4c47fc4f
minkhant21 <kaungminkhant6889@gmail.com>
2023-06-27 21:25:52 +0630

    homePageDesign

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+function Hero(props) {
+    
+
+    return (
+        <div className=" h-60 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 flex items-center justify-center text-center">
+        <div className="space-y-2">
+           <h1 className="text-3xl text-white">Welcome From Books Store</h1>
+            <p className="text-white">A place where you can store and manage your books</p>
+        </div>
+      </div>
+    )
+}
+
+export default Hero
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,30 @@
 import React from "react";
-
+import books from  '../assets/books.jpeg'
+import Hero from "../components/Hero";
 function Home ()
 {
   return(
-    <div>
-      <h2 className=" text-fuchsia-500">Hello world</h2>
-    </div>
+    <>
+        <Hero/>
+        <div className='grid grid-cols-2 md:grid-cols-4 gap-4 my-3'>
+          {[1, 2, 3, 4, 5].map(() => (
+            <div className='p-4 border border-1'>
+
+              <img src={books} alt="" className="w-full" />
+              <div className='text-center space-y-2 mt-3'>
+                <h1>Book title</h1>
+                <p>description</p>
+                {/* genres */}
+                <div className='flex flex-wrap'>
+                  {['travel', 'knowledge', 'travel', 'knowledge'].map(genre => (
+                    <span className='mx-1 my-1 text-white rounded-full px-2 py-1 text-sm bg-blue-500'> {genre}</span>
+                  ))}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+    </>
   )
 }
 
